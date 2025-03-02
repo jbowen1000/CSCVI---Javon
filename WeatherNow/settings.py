6333ef0a4077a0e3cd21703d2759602a3d7f1bfe
Javon Bowen <javonbowen@Javons-MacBook-Air.local>
2025-03-02 13:32:25 -0500

    Update settings.py and urls.py

--- WeatherNow/settings.py
+++ WeatherNow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k!0rc^@7wktt28i4)xi_8cv!$syf_4vo2rsk=83yjf!9wk7$ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'WeatherNow.urls'
 
+# settings.py
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'weather' / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
