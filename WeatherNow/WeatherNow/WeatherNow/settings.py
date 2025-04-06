155f1d34c82512a64444544e7c65d415b9690926
Javon Bowen <javonbowen@Javons-MacBook-Air.local>
2025-04-06 14:18:36 -0400

    Added styles.css with basic styling for pages

--- WeatherNow/WeatherNow/WeatherNow/settings.py
+++ WeatherNow/WeatherNow/WeatherNow/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'WeatherNow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'myapp/templates'],  
+        'DIRS': [BASE_DIR / 'myapp' / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'weather/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
